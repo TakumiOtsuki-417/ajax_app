67f0251d72489b5aa05776c660913c727ffab8f7
TakumiOtsuki-417 <heitaku443@gmail.com>
2021-06-03 16:48:30 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,19 @@
+const buildHTML = (XHR) => {
+  // HTMLを生成する処理をまとめる
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+    //HTML出力の結果をまとめた変数htmlを返却
+    return html;
+};
+
 function post (){
   //リクエストを送信する処理をかく
   const submit = window.document.getElementById("submit");
@@ -9,6 +25,18 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        //通信に失敗した時に発動
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      //挿入HTMLの引数には、関数buildHTMLの返り値を渡す
+      list.insertAdjacentHTML('afterend', buildHTML(XHR));
+      formText.value = "";
+    };
   });
 }
 
